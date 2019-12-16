375ac7be3a78a062518a96eb4bb8068656f4bfb9
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-12-16 20:02:05 +1100

    add project

--- js/app.js
+++ js/app.js
@@ -2,15 +2,14 @@
 /*             Fullpage.js Calling Function      */
 /* ============================================= */
 
-$(document).ready(function() {
+$(document).ready(function () {
 	$('#fullpage').fullpage({
 		//options here
-		autoScrolling:true,
-        scrollHorizontally: true,
-        
-        easingcss3: 'ease',
-	});
+		autoScrolling: true,
+		scrollHorizontally: true,
 
+		easingcss3: 'ease',
+	});
 	//methods
 	$.fn.fullpage.setAllowScrolling(true);
 });
@@ -18,34 +17,60 @@ $(document).ready(function() {
 /* ============================================= */
 /*             Projects Modal Function           */
 /* ============================================= */
-
-const projects ={
-	"projects":[
+const projects = {
+	"projects": [
 		{
-			"modal-title":"Responsive Website",
-			"img":"images/projectsUI-01.jpg",
-			"description":"Build a responsive,mobile-first layout using HTML and CSS. This layout demonstrates my understanding of responsive design by adjusting to accommodate small, medium and large screen sizes. I wrote CSS to style the page for the small mobile device first and then use min-width media queries. I also add breakpoints to adjust the layout for wider tablet and desktop screens.",
-			"skills":"HTML5, CSS, JavaScript",
-			"preview-link":"https://jiwu6498.github.io/Techdegree-Project2/",
-			"github-link":"https://github.com/JIWU6498/Techdegree-Project2",
-
-		},
-		{
-			"modal-title":"Responsive Website",
-			"img":"images/projectsUI-01.jpg",
-			"description":"Build a responsive,mobile-first layout using HTML and CSS. This layout demonstrates my understanding of responsive design by adjusting to accommodate small, medium and large screen sizes. I wrote CSS to style the page for the small mobile device first and then use min-width media queries. I also add breakpoints to adjust the layout for wider tablet and desktop screens.",
-			"skills":"HTML5, CSS, JavaScript",
-			"preview-link":"https://jiwu6498.github.io/Techdegree-Project2/",
-			"github-link":"https://github.com/JIWU6498/Techdegree-Project2",
+			"title": "Responsive Website",
+			"img": "images/project2.png",
+			"description": "Build a responsive,mobile-first layout using HTML and CSS. This layout demonstrates my understanding of responsive design by adjusting to accommodate small, medium and large screen sizes. I wrote CSS to style the page for the small mobile device first and then use min-width media queries. I also add breakpoints to adjust the layout for wider tablet and desktop screens.",
+			"skills": "HTML5, CSS, JavaScript",
+			"preview": "https://jiwu6498.github.io/Techdegree-Project2/",
+			"github": "https://github.com/JIWU6498/Techdegree-Project2",
 
 		},
 		{
-			"modal-title":"Responsive Website",
-			"img":"images/projectsUI-01.jpg",
-			"description":"Build a responsive,mobile-first layout using HTML and CSS. This layout demonstrates my understanding of responsive design by adjusting to accommodate small, medium and large screen sizes. I wrote CSS to style the page for the small mobile device first and then use min-width media queries. I also add breakpoints to adjust the layout for wider tablet and desktop screens.",
-			"skills":"HTML5, CSS, JavaScript",
-			"preview-link":"https://jiwu6498.github.io/Techdegree-Project2/",
-			"github-link":"https://github.com/JIWU6498/Techdegree-Project2",
+			"title": "Web Style Guide Study",
+			"img": "images/projectsUI-01.jpg",
+			"description": "Using SASS to pre-process the css styles to format the page",
+			"skills": "HTML5, CSS, SASS",
+			"preview": "https://jiwu6498.github.io/Techdegree-Project4/",
+			"github": "https://github.com/JIWU6498/Techdegree-Project4",
+
+		},
+		{
+			"title": "Interactive Photo Gallery",
+			"img": "images/projectsUI-01.jpg",
+			"description": "Using jQuery plugin to create a interative photo gallery",
+			"skills": "HTML5, CSS, SASS, JavaScript, jQuery plugin",
+			"preview": "https://jiwu6498.github.io/Techdegree-Project5/",
+			"github": "https://github.com/JIWU6498/Techdegree-Project5",
+
+		},
+		{
+			"title": "Game Show App",
+			"img": "images/projectsUI-01.jpg",
+			"description": "Create a browser version of “Wheel of Success”, a word guessing game where players will click letters from an onscreen keyboard to try to guess a random phrase.",
+			"skills": "HTML5, CSS, JavaScript, DOM Scripting ,CSS transition",
+			"preview": "https://jiwu6498.github.io/Techdegree-Project6/",
+			"github": "https://github.com/JIWU6498/Techdegree-Project6",
+
+		},
+		{
+			"title": "WebApp Dashboard",
+			"img": "images/projectsUI-01.jpg",
+			"description": "Built a web dashboard based on a mockup with JavaScript-driven charts and graphs.",
+			"skills": "HTML5, CSS, SASS,CSS Transition, JavaScript ,jQury , DOM, Chart.js",
+			"preview": "https://jiwu6498.github.io/Techdegree-Project7/",
+			"github": "https://github.com/JIWU6498/Techdegree-Project7",
+
+		},
+		{
+			"title": "API Employee Directory",
+			"img": "images/projectsUI-01.jpg",
+			"description": "Communicating with APIs which allows to work with microservices and with vast databases to build useful tools and relevant information quickly and easily.Request a JSON object from the API using fetch and parse the data so that 12 employees are listed in a grid with their thumbnail image, full name, email, and location.",
+			"skills": "JavaScript, Promise, JSON, API",
+			"preview": "https://jiwu6498.github.io/Techdegree-Project8",
+			"github": "https://github.com/JIWU6498/Techdegree-Project8",
 
 		},
 
@@ -53,3 +78,38 @@ const projects ={
 
 }
 
+const viewButtons = document.getElementsByClassName('viewBtn');
+const modalContainer = document.getElementById('modalContainer');
+
+for (let i = 0; i < viewButtons.length; i++) {
+	viewButtons[i].addEventListener('click', () => {
+
+		const modal = document.getElementById("projectsModal");
+
+		modal.innerHTML = `
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="projectsModalLabel">${projects.projects[i].title}</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<img id="projectImg" src=${projects.projects[i].img} class="card-img-top text-secondary rounded" alt=${projects.projects[i].img} >
+					<h5 class="my-3 text-uppercase text-secondary">Description:</h5>
+					<p>${projects.projects[i].description}</p>
+					<h5 class="my-3 text-uppercase">Skills:</h5>
+					<p>${projects.projects[i].skills}</p>  
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<a href=${projects.projects[i].preview}  class="btn btn-primary" target="_blank">Preview</a>
+					<a href=${projects.projects[i].github}   class="btn btn-primary" target="_blank">GitHub Repository</a>
+				</div>
+			</div>
+		</div>
+		`
+		modalContainer.appendChild(modal);
+	})
+}
